8b464ac49a353beb5763a7cbab8e5511dd24c15d
Jim Thomason <jim@japanesewartuba.com>
2021-02-11 16:05:33 -0600

    jimt/easybase-intro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+// npm install easybase-react
+import React, { useState, useEffect } from "react";
+
+import { EasybaseProvider, useEasybase } from "easybase-react";
+import ebconfig from "./ebconfig.js";
+
+function Container() {
+  const { Frame, useFrameEffect, configureFrame, sync } = useEasybase();
+
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState(0);
+  const [othertext, setOtherText] = useState("");
+
+  useEffect(() => {
+    configureFrame({ limit: 10, offset: 0, tableName: "LISTOFDATA" });
+    sync();
+    // eslint-disable-next-line
+  }, []);
+
+  useFrameEffect(() => {
+    console.log("Frame changed!");
+  });
+
+  return (
+    <div>
+      {Frame().map((ele, index) => (
+        <div>
+          {ele.name}, {ele.number}, {ele.othertext}
+        </div>
+      ))}
+      <div>
+        <div>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          Number:
+          <input
+            type="number"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+          />
+        </div>
+        <div>
+          Other Text:
+          <input
+            type="text"
+            value={othertext}
+            onChange={(e) => setOtherText(e.target.value)}
+          />
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              Frame().push({
+                name,
+                number,
+                othertext,
+              });
+              sync();
+              setName("");
+              setNumber(0);
+              setOtherText("");
+            }}
+          >
+            Save values
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <EasybaseProvider ebconfig={ebconfig}>
+      <Container />
+      <Container />
+    </EasybaseProvider>
   );
 }
 
